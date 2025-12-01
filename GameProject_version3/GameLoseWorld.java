a164a37f394ac80a47273893608bfd4b6d925ca3
Jacob Boucher <2535431@edu.vaniercollege.qc.ca>
2025-12-01 12:44:50 -0500

    You can retrun to menu after losing

--- GameProject_version3/GameLoseWorld.java
+++ GameProject_version3/GameLoseWorld.java
@@ -1,4 +1,3 @@
-// WARNING: This file is auto-generated and any changes to it will be overwritten
 import lang.stride.*;
 import java.util.*;
 import greenfoot.*;
@@ -8,13 +7,15 @@ import greenfoot.*;
  */
 public class GameLoseWorld extends World
 {
-
+    double loseWorldScreenCreation = System.currentTimeMillis();
     /**
      * Constructor for objects of class GameLoseWorld.
      */
     public GameLoseWorld()
     {
         super(624, 520, 1);
+        showText("Press 'space' button to return to menu", 290, 500);
+        prepare();
     }
 
     /**
@@ -23,9 +24,31 @@ public class GameLoseWorld extends World
     public void showTextWithBigBlackFont(String message, int x, int y)
     {
         GreenfootImage bg = getBackground();
-        Font font =  new Font(250);
+        Font font =  new  Font(250);
         bg.setFont(font);
         bg.setColor(Color.WHITE);
         bg.drawString(message, x, y);
     }
+
+    public void act()
+    {
+        int diff = (int) (System.currentTimeMillis() - loseWorldScreenCreation);
+        int timerValue = (int) (System.currentTimeMillis() - loseWorldScreenCreation)/1000;
+        if (Greenfoot.isKeyDown("space"))
+        {
+            Greenfoot.setWorld(new MenuScreen()); 
+            Greenfoot.playSound("ClickSplashScreens(high_volume).wav");
+        }
+
+    }
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+    }
 }
+
+      
+    
